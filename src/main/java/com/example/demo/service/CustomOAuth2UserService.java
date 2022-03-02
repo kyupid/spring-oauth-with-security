755b7f6324abf9ccb199f7c33121d9a1b3cf40bd
kyupid <iamscvv@gmail.com>
2022-03-02 17:13:41 +0900

    oauth 로그인시 db에 유저정보 저장하지 않고 세션에만 둔다
    - 로그인 성공시 추가정보 페이지로 갈건데 그 페이지에서 성공적으로 모든 폼을 입력하고 제출해야 성공적으로 회원가입이 가능
    - 만약 해당 페이지에서 벗어나면 세션 삭제해서 접근 권한 없도록 함

--- src/main/java/com/example/demo/service/CustomOAuth2UserService.java
+++ src/main/java/com/example/demo/service/CustomOAuth2UserService.java
@@ -49,8 +49,6 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                 .map(entity -> entity.update(attributes.getName(),attributes.getPicture()))
                 .orElse(attributes.toEntity());
 
-        System.out.println(user.toString());
-
-        return userRepository.save(user);
+        return user;
     }
 }
